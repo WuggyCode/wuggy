f94a738df0d5a97aafa29f677bef22226a9dde69
Zenulous <zenvanriel@gmail.com>
2020-10-15 14:30:03 +0200

    Updated demo code

---
+++ sample.py
@@ -0,0 +1,11 @@
+from wuggy.generators.wuggygenerator import WuggyGenerator
+import re
+import time
+from fractions import Fraction
+import random
+import sys
+
+g = WuggyGenerator()
+g.load("orthographic_english")
+for sequence in g.generate_simple("trumpet"):
+    print(sequence)

70ded6a75029122cdb020e5c7c3c1b209da7c54c
Zenulous <zenvanriel@gmail.com>
2020-10-28 16:36:03 +0100

    Init cleanup + linting

--- wuggy/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-The Wuggy Python package provides classes and methods to generate pseudowords using the Wuggy method 
-
-[Keuleers, E., & Brysbaert, M. (2010). Wuggy: A multilingual pseudoword generator. Behavior Research Methods 42(3), 627-633](http://crr.ugent.be/papers/Wuggy_BRM.pdf)
-"""

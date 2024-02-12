339c47ecc5e12d59c3b10763582fbb4e08934f20
zenulous <zenvanriel@gmail.com>
2024-02-12 17:20:17 +0100

    Less strict requirements

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="wuggy",
-    version="1.1.0",
+    version="1.1.1",
     author="Emmanuel Keuleers",
     author_email="E.A.Keuleers@tilburguniversity.edu",
     description="Wuggy: A multilingual pseudoword generator",
